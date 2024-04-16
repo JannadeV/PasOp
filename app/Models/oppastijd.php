2486754ec28c83c9e6299e31f6883a8f6ed8deb9
JannadeV <jannavries98@gmail.com>
2024-04-16 15:57:51 +0200

    models

---
+++ app/Models/oppastijd.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class oppastijd extends Model
+{
+    use HasFactory;
+}
