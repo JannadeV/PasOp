beac10d1595a61124be7a737a53fdb8ce976aa46
JannadeV <jannavries98@gmail.com>
2024-04-16 15:58:06 +0200

    seeders

---
+++ database/seeders/OppastijdSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OppastijdSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
