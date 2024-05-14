b96a687be87b7ca4cafc014caa554970c27167dd
JannadeV <jannavries98@gmail.com>
2024-05-14 12:37:44 +0200

    debug for tailwind

--- resources/views/components/huisdier/card.blade.php
+++ resources/views/components/huisdier/card.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
-    <p className="text-3xl font-bold underline">
+    <p class="text-3xl font-bold underline">
         Hello world! <br>
     </p>
-    <img src="{{ asset($pet->dierfotos[0]->path) }}"  class="card-img-top h-full w-full object-cover object-center lg:h-full lg:w-full" alt="Foto van een huisdier" >
+    <img class="card-img-top h-full w-full object-cover object-center lg:h-full lg:w-full" src="{{ asset($pet->dierfotos[0]->path) }}" alt="Foto van een huisdier" >
     <!--<img src="{{ asset('img/myimage.png') }}" alt="description of myimage">-->
     <div class="card-body">
         <h5 class="card-title">{{ $pet->naam }}</h5>
