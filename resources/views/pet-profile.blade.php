543cd5539a4d7d997d7df5177271e9c35f6a859f
JannadeV <jannavries98@gmail.com>
2024-05-17 18:28:17 +0200

    carousel in other file for dry programming, per-profile

--- resources/views/pet-profile.blade.php
+++ resources/views/pet-profile.blade.php
@@ -1,47 +1,28 @@
-<x-app-layout>
-<div id="indicators-carousel" class="relative w-full" data-carousel="static">
-    <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-
-        @foreach($huisdier->dierfotos as $foto)
-        @if($foto = $huisdier->dierfotos[0])
-        <div class="duration-700 ease-in-out" data-carousel-item="active">
-            <img src={{ asset($foto->path) }} class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
+<x-app-layout :header="$huisdier->naam">
+    <div class="grid grid-cols-12">
+        <x-image-carousel :fotos="$huisdier->dierfotos" class="col-start-1 col-end-13 h-72"></x-image-carousel>
+        <p class="col-start-2 col-end-12 pt-1">Bio <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel lectus ac tellus tincidunt suscipit sit amet ac sapien.</p>
+        <p class="col-start-2 col-end-12 pt-1 font-bold">Oppastijden</p>
+        <div class="col-start-2 col-end-12 flex flex-col border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600">
+            <ul class="grid grid-cols-2 dark:text-white border-b border-gray-300">
+                @foreach($huisdier->oppastijds as $index=>$oppastijd)
+                    @if($index%2 == 0)
+                        @php($border_side = "border-r")
+                    @else
+                        @php($border_side = "")
+                    @endif
+                    <li class="col-span-1 {{$border_side}} border-gray-300 dark:border-gray-600">
+                        <div class="flex items-center ps-3">
+                            <input id="vue-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                            <label for="vue-checkbox" class="py-3 ms-2 dark:text-gray-300">
+                                <p class="text-xs">{{ date("d-m-'y", strtotime($oppastijd->datum))}}</p>
+                                <p class="text-xs">{{ substr($oppastijd->start, 0, 5) }} - {{ substr($oppastijd->eind, 0, 5) }}</p>
+                            </label>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+            <x-primary-button class="justify-self-center self-center m-1">Pas op!</x-primary-button>
         </div>
-        @else
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset($foto->path) }}" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-        @endif
-        @endforeach
     </div>
-
-    <!-- Slider indicators -->
-    <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
-        @for($i=0; $i<count($huisdier->dierfotos); $i++ )
-        @if($i == 0)
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide {{$i + 1}}" data-carousel-slide-to="{{ $i }}"></button>
-        @else
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide {{$i + 1}}" data-carousel-slide-to="{{ $i }}"></button>
-        @endif
-        @endfor
-    </div>
-    <!-- Slider controls -->
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Next</span>
-        </span>
-    </button>
-</div>
-
 </x-app-layout>
