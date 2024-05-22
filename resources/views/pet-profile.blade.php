79fe9f83a9bfbab6c3903ff821e97ef1b0bb77a1
JannadeV <jannavries98@gmail.com>
2024-05-22 11:47:50 +0200

    better lay-out for profile

--- resources/views/pet-profile.blade.php
+++ resources/views/pet-profile.blade.php
@@ -23,14 +23,14 @@
             }
         }
     </script>
-        <x-slot name="header">
-            <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ $huisdier->naam }}
-            </h2>
-        </x-slot>
-        <div class="grid grid-cols-12">
+    <x-slot name="header">
+        <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ $huisdier->naam }}
+        </h2>
+    </x-slot>
+    <div class="grid grid-cols-12">
         <x-image-carousel :fotos="$huisdier->dierfotos" class="col-start-1 col-end-13 h-72"></x-image-carousel>
-        <p class="col-start-2 col-end-12 pt-1">Bio <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel lectus ac tellus tincidunt suscipit sit amet ac sapien.</p>
+        <p class="col-start-2 col-end-12 pt-2">Bio Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vel lectus ac tellus tincidunt suscipit sit amet ac sapien.</p>
         <p class="col-start-2 col-end-12 pt-1 font-bold">Oppastijden</p>
         <div class="col-start-2 col-end-12 flex flex-col border border-gray-300 rounded-lg dark:bg-gray-700 dark:border-gray-600">
             <form onsubmit="return validateForm()" action="{{ route('aanvragen.store') }}" method="POST" class="grid grid-cols-1 dark:text-white border-b border-gray-300">
@@ -41,7 +41,7 @@
                     @else
                         @php($border_side = "")
                     @endif
-                    <li class="col-span-1 {{$border_side}} border-gray-300 dark:border-gray-600">
+                    <div class="col-span-1 {{$border_side}} border-gray-300 dark:border-gray-600">
                         <div class="flex items-center ps-3">
                             <input type="hidden" name="tijden[{{ $index }}][id]" value="{{ $oppastijd->id }}">
                             <input type="checkbox" name="tijden[{{ $index }}][selected]" value="1" id="vue-checkbox{{ $index }}" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
@@ -50,7 +50,7 @@
                                 <p class="text-xs">{{ substr($oppastijd->start, 0, 5) }} - {{ substr($oppastijd->eind, 0, 5) }}</p>
                             </label>
                         </div>
-                    </li>
+                    </div>
                 @endforeach
                 <x-primary-button type="submit" class="justify-self-center self-center m-1">Pas op!</x-primary-button>
             </form>
