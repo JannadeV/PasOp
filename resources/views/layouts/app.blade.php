f1030bca6309651aff0698d2c41af4e4d80d3ade
JannadeV <jannavries98@gmail.com>
2024-05-22 11:53:51 +0200

    being able to log in in every page that uses app-lay-out

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,8 +24,25 @@
             <!-- Page Heading -->
             @if (isset($header))
                 <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <div class="flex flex-row items-center max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
+                        @if (Route::has('login'))
+                            <nav class="-mx-3 flex flex-1 justify-end">
+                                @guest
+                                    <a  href="{{ url('/dashboard') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >   Dashboard</a>
+                                    <a  href="{{ route('login') }}"
+                                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                    >   Log in</a>
+                                    @if (Route::has('register'))
+                                        <a  href="{{ route('register') }}"
+                                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                        >   Register</a>
+                                    @endif
+                                @endguest
+                            </nav>
+                        @endif
                     </div>
                 </header>
             @endif
