f35bf74d23001a0ab2323f5649e2158100813043
JannadeV <jannavries98@gmail.com>
2024-05-24 00:17:51 +0200

    clearer that javascript is seperate

--- resources/views/pet-profile.blade.php
+++ resources/views/pet-profile.blade.php
@@ -1,4 +1,5 @@
-<x-app-layout>
+<x-app-layout class="overflow-scroll">
+
     <script>
         function validateForm () {
             // Zoek alle checkboxen
@@ -23,6 +24,7 @@
             }
         }
     </script>
+
     <x-slot name="header">
         <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ $huisdier->naam }}
