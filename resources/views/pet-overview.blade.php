f44debb453060bbfe6ad2448babd2b853e79c710
JannadeV <jannavries98@gmail.com>
2024-05-24 00:21:40 +0200

    hopefully i can scroll again in the pages where you should be able to scroll

--- resources/views/pet-overview.blade.php
+++ resources/views/pet-overview.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout class="overflow-scroll">
     <x-slot name="header">
         <h2 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Pet Overview') }}
