9304424e75a260e32776791452c389454f981ce0
JannadeV <jannavries98@gmail.com>
2024-07-03 14:58:29 +0200

    dierfotocontroller has a store function that calles the fotocontroller store function for DRY and that makes sure the dierfoto is set right in the database

---
+++ app/Http/Controllers/DierfotoController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dierfoto;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+
+class DierfotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // Verwerk de foto met FotoController
+        $response = Http::asForm()->post(url('/upload'), [
+            'foto' => $request->file('dierfoto')
+        ]);
+        $path = $response->json('path');
+
+        //Valideer de rest
+        $validated = $request->validate([
+            'huisdierId' => 'required|integer',
+        ]);
+
+        $dierfoto = Dierfoto::create([
+            'path' => $path,
+            'huisdier_id' => $validated['huisdierId'],
+        ]);
+        $dierfoto->huisdier()->sync($request->huisdierId);
+
+        return redirect()->route('pet.show', ['id' => $validated['huisdierId']]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(foto $foto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(foto $foto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, foto $foto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(foto $foto)
+    {
+        //
+    }
+}
