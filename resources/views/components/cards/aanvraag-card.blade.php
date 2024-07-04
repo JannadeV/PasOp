558e4eccbb84f341026aaaf13b9521802472bbf5
JannadeV <jannavries98@gmail.com>
2024-07-04 14:46:04 +0200

    made a standard card for DRYness and put the cards in a seperate directory

---
+++ resources/views/components/cards/aanvraag-card.blade.php
@@ -0,0 +1,32 @@
+<x-cards.general-card
+    :bigFotoPath="asset($aanvraag->huisfotos[0]->path)"
+    :bigFotoAlt="'foto van het huis van de oppasser'"
+    :title="$aanvraag->oppasser->name . ' wil op ' . $huisdier->naam . ' passen'">
+
+    <x-slot name="topRight">
+        <img class="w-24 h-24 shadow rounded-full aspect-square object-cover"
+             src="{{ asset($huisdier->dierfotos[0]->path) }}"
+             alt="foto van het op te passen dier"/>
+    </x-slot>
+
+    <x-slot name="middle">
+        <div class="max-h-24 overflow-scroll">
+            @foreach ($aanvraag->oppastijds as $oppastijd)
+            <div class="p-2 border border-solid">
+                <p class="text-xs">{{ date("d-m-'y", strtotime($oppastijd->datum))}}</p>
+                <p class="text-xs">
+                    {{ substr($oppastijd->start, 0, 5) }} - {{ substr($oppastijd->eind, 0, 5) }}
+                </p>
+            </div>
+            @endforeach
+        </div>
+    </x-slot>
+
+    <x-slot name="button">
+        <a href="{{ route('aanvragen.show', ['id' => $aanvraag->id]) }}">
+            <x-button.primary-button>Naar aanvraag</x-button.primary-button>
+        </a>
+    </x-slot>
+
+</x-cards.general-card>
+
---
+++ resources/views/components/cards/pet-card.blade.php
@@ -0,0 +1,58 @@
+@include('popper::assets')
+<x-cards.general-card
+    :bigFotoPath="asset($huisdier->dierfotos[0]->path)"
+    :bigFotoAlt=" 'Foto van een huisdier' "
+    :title="$huisdier->naam">
+
+    <x-slot name="topRight">
+        @switch( strtolower($huisdier->soort) )
+        @case("hond")
+            <i class="fa-solid fa-dog text-gray-700 text-4xl"></i>
+            @break
+        @case("kat")
+            <i class="fa-solid fa-cat text-gray-700 text-4xl"></i>
+            @break
+        @case("vogel")
+            <i class="fa-solid fa-feather text-gray-700 text-4xl"></i>
+            @break
+        @case("paard")
+            <i class="fa-solid fa-horse text-gray-700 text-4xl"></i>
+            @break
+        @case("vis")
+            <i class="fa-solid fa-fish text-gray-700 text-4xl"></i>
+            @break
+        @default
+            <i class="fa-solid fa-question text-gray-700 text-4xl"></i>
+        @endswitch
+    </x-slot>
+
+    <x-slot name="middle">
+        @auth
+        <div class="max-h-24 overflow-scroll">
+            @foreach ($huisdier->oppastijds as $oppastijd)
+            <div class="p-2 border border-solid">
+                <p class="text-xs">{{ date("d-m-'y", strtotime($oppastijd->datum))}}</p>
+                <p class="text-xs">{{ substr($oppastijd->start, 0, 5) }} - {{ substr($oppastijd->eind, 0, 5) }}</p>
+            </div>
+            @endforeach
+        </div>
+        @endauth
+        @guest
+        <p>Log in om de oppastijden te zien.</p>
+        @endguest
+    </x-slot>
+
+    <x-slot name="button">
+        @auth
+        <a href="{{ route('pet.show', ['id' => $huisdier->id]) }}">
+            <x-button.primary-button class="justify-self-end">Naar profiel</x-button.primary-button>
+        </a>
+        @endauth
+        @guest
+        <x-button.disabled-button tooltip="log eerst in" class="justify-self-end">
+            Naar profiel
+        </x-button.disabled-button>
+        @endguest
+    </x-slot>
+
+</x-cards.general-card>
