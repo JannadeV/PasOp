552ea52ee6b3d8dfdd624f1be84fa973e73ddb32
JannadeV <jannavries98@gmail.com>
2024-07-11 13:30:41 +0200

    being able to log out in profile page in stead of in navigtion

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -71,26 +71,18 @@
         @auth
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
 
             <div class="mt-3 space-y-1">
-                <x-navigation.responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                <x-navigation.responsive-nav-link :href="route('dashboard')">
+                    Dashboard
+                </x-navigation.responsive-nav-link>
+                <x-navigation.responsive-nav-link :href="route('huisdier.overview')">
+                    Huisdieroverzicht
                 </x-navigation.responsive-nav-link>
 
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-navigation.responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-navigation.responsive-nav-link>
-                </form>
+                <x-navigation.responsive-nav-link :href="route('profile.edit')">
+                    {{ __('Profiel') }}
+                </x-navigation.responsive-nav-link>
             </div>
         </div>
         @endauth
