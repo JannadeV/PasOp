2aa93c1723dac517d6bdc67f4b256fa2ea38df21
JannadeV <jannavries98@gmail.com>
2025-01-18 19:57:00 +0100

    grotere hondjes op grotere pagina

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-guest-layout>
     <main class="relative h-full">
-        <img class="absolute z-40 w-20 md:w-32 h-auto left-4 -top-8" src="img/dweilhond.png" alt="hond met lang haar">
-        <img class="absolute z-40 w-20 md:w-32 h-auto right-6 top-1/2" src="img/poolhond.png" alt="poolhond">
+        <img class="absolute z-40 w-20 md:w-24 lg:w-32 h-auto left-4 -top-8" src="img/dweilhond.png" alt="hond met lang haar">
+        <img class="absolute z-40 w-20 md:w-32 lg:w-40 h-auto right-6 top-1/2" src="img/poolhond.png" alt="poolhond">
 
         <div class="relative landscape:hidden grid grid-welkom-portait h-full">
             <x-inloggen class="h-full"></x-inloggen>
