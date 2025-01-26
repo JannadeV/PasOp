5708f0029114ab6ec6c9372ae4e57b50c9baeed2
JannadeV <jannavries98@gmail.com>
2025-01-26 17:37:37 +0100

    hopefully being able to merge properly from main

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,71 +1,95 @@
-<x-app-layout class="overflow-scroll">
+<x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h1 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
-        </h2>
+        </h1>
     </x-slot>
 
-    <div class="w-full p-3">
-        <h2 class="text-lg">Over mij</h2>
-        <p class="text-sm ml-1">Naam: {{ $user->name }}</p>
-        <p class="text-sm ml-1">Email: {{ $user->email }}</p>
-    </div>
+    <main class="relative">
 
-    <x-dashboard-section header="Mijn huisdieren" :count="count($huisdieren)" :action="route('huisdier.create')">
-        <x-slot name="cards">
-            @if(count($huisdieren) > 0)
-            @foreach ($huisdieren as $huisdier)
-                <x-cards.pet-card :huisdier="$huisdier"/>
-            @endforeach
-            @endif
-        </x-slot>
-    </x-dashboard-section>
+        <div class="flex w-screen pb-2">
+            <div class="p-3 flex-1">
+                <h3 class="text-xl">Over mij</h3>
+                <div class="flex text-sm flex-nowrap">
+                    <span class="font-semibold">Naam:</span>
+                    <span class="ml-2">{{ $user->name }}</span>
+                </div>
+                <div class="flex text-sm flex-nowrap">
+                    <span class="font-semibold">Email:</span>
+                    <span class="ml-2">{{ $user->email }}</span>
+                </div>
+            </div>
 
-    @if(count($aanvragen) > 0)
-    <x-dashboard-section header="Openstaande oppasaanvragen" :count="count($aanvragen)">
-        <x-slot name="cards">
-            @foreach($aanvragen as $aanvraag)
-            <x-cards.aanvraag-card
-                :aanvraag="$aanvraag"
-                :huisdier="$aanvraag->oppastijds->first()->huisdier"/>
-            @endforeach
-        </x-slot>
-    </x-dashboard-section>
-    @endif
+            <div class="flex-shrink px-8 max-w-xs">
+                <img class="max-w-full h-auto relative scale-x-[-1]" src="img/kwispel-hond.gif" alt="kwispelende hond">
+            </div>
+        </div>
 
-    @if(count($afspraken) > 0)
-    <x-dashboard-section header="Geaccepteerde oppasaanvragen" :count="count($afspraken)">
-        <x-slot name="cards">
-            @foreach($afspraken as $afspraak)
-            <x-cards.aanvraag-card
-                :aanvraag="$afspraak"
-                :huisdier="$afspraak->oppastijds->first()->huisdier"/>
-            @endforeach
-        </x-slot>
-    </x-dashboard-section>
-    @endif
+        <x-dashboard-section header="Mijn huisdieren" :count="count($huisdieren)">
+            <x-slot name="action">
+                <a href="{{ route('huisdier.create') }}">
+                    <x-button.small-secondary-button class="p-2 mx-4">
+                        <i class="fa-solid fa-plus"></i>
+                    </x-button.small-secondary-button>
+                </a>
+            </x-slot>
 
-    @if(count($aangeboden) > 0)
-    <x-dashboard-section header="Aangeboden oppasaanvragen" :count="count($aangeboden)">
-        <x-slot name="cards">
-            @foreach($aangeboden as $aanbod)
-            <x-cards.aanvraag-card
-                :aanvraag="$aanbod"
-                :huisdier="$aanbod->oppastijds->first()->huisdier"/>
-            @endforeach
-        </x-slot>
-    </x-dashboard-section>
-    @endif
+            <x-slot name="cards">
+                @if(count($huisdieren) > 0)
+                @foreach ($huisdieren as $huisdier)
+                    <x-cards.pet-card :huisdier="$huisdier"/>
+                @endforeach
+                @endif
+            </x-slot>
+        </x-dashboard-section>
+
+        @if(count($aanvragen) > 0)
+        <x-dashboard-section header="Openstaande oppasaanvragen" :count="count($aanvragen)">
+            <x-slot name="cards">
+                @foreach($aanvragen as $aanvraag)
+                <x-cards.aanvraag-card
+                    :aanvraag="$aanvraag"
+                    :huisdier="$aanvraag->oppastijds->first()->huisdier"/>
+                @endforeach
+            </x-slot>
+        </x-dashboard-section>
+        @endif
+
+        @if(count($afspraken) > 0)
+        <x-dashboard-section header="Geaccepteerde oppasaanvragen" :count="count($afspraken)">
+            <x-slot name="cards">
+                @foreach($afspraken as $afspraak)
+                <x-cards.aanvraag-card
+                    :aanvraag="$afspraak"
+                    :huisdier="$afspraak->oppastijds->first()->huisdier"/>
+                @endforeach
+            </x-slot>
+        </x-dashboard-section>
+        @endif
+
+        @if(count($aangeboden) > 0)
+        <x-dashboard-section header="Aangeboden oppasaanvragen" :count="count($aangeboden)">
+            <x-slot name="cards">
+                @foreach($aangeboden as $aanbod)
+                <x-cards.aanvraag-card
+                    :aanvraag="$aanbod"
+                    :huisdier="$aanbod->oppastijds->first()->huisdier"/>
+                @endforeach
+            </x-slot>
+        </x-dashboard-section>
+        @endif
 
 
-    @if(count($reviews) > 0)
-    <x-dashboard-section header="Mijn reviews" :count="count($reviews)">
-        <x-slot name="cards">
-            @foreach($reviews as $review)
-            <x-cards.review-card :review="$review"/>
-            @endforeach
-        </x-slot>
-    </x-dashboard-section>
-    @endif
+        @if(count($reviews) > 0)
+        <x-dashboard-section header="Mijn reviews" :count="count($reviews)">
+            <x-slot name="cards">
+                @foreach($reviews as $review)
+                <x-cards.review-card :review="$review"/>
+                @endforeach
+            </x-slot>
+        </x-dashboard-section>
+        @endif
 
+
+    </main>
 </x-app-layout>
