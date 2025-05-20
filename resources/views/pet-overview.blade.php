fcf894c6a3b7a46d7e3bd39e2be5bc282806609f
JannadeV <jannavries98@gmail.com>
2025-05-20 21:33:16 +0200

    blubbie nu ook mooi qua responsive

--- resources/views/pet-overview.blade.php
+++ resources/views/pet-overview.blade.php
@@ -7,7 +7,7 @@
     </x-slot>
 
     <!--pet cards-->
-    <div class="mx-2 p-1 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
+    <div class="mx-2 p-1 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-1">
         @foreach ($huisdiers as $huisdier)
         <div class="col-span-1">
             <x-cards.pet-card :huisdier="$huisdier"
